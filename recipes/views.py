9afd3b484c75d4134defa7a8dae8fe3900c24c90
IngridEsteves <ingridestevespereiraiep@gmail.com>
2023-04-28 19:02:15 -0300

    remove-contato-sobre

--- recipes/views.py
+++ recipes/views.py
@@ -6,15 +6,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/home.html')
     # return HTTP Response
-
-
-# HTTP Request
-def sobre(request):
-    return render(request, 'recipes/sobre.html')  
-    # return HTTP Response
-
-
-# HTTP Request
-def contato(request):
-    return render(request, 'recipes/contato.html')
-    # return HTTP Response
