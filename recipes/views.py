f1d2b1d0bea8b73be094bb5a27187be1dc970937
IngridEsteves <ingridestevespereiraiep@gmail.com>
2023-06-27 19:41:07 -0300

    page-receitas

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 # HTTP Request
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={'name': 'Ingrid Esteves'})
+    # return HTTP Response
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Ingrid Esteves'})
     # return HTTP Response
