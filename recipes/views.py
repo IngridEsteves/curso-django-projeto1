6f6d80f450de97e4d512abb097502d88c6a49489
IngridEsteves <ingridestevespereiraiep@gmail.com>
2023-06-29 23:57:27 -0300

    models-orm

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 from .models import Recipe
 
@@ -7,19 +6,26 @@ from .models import Recipe
 # Create your views here.
 # HTTP Request
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True,).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes, })
     # return HTTP Response
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes, })
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Categoria | '
+    })
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
     # return HTTP Response
